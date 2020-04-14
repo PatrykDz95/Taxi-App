d29cbe3e3b0ab30d98fb63dbdaab418f7a023200
Patryk <patrykdziedzioch@gmail.com>
2020-04-14 20:09:56 +0200

    Made changes for the structures sake

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+C
+UberApp/settings.py,4\a\4a3930e0ce56c70d9cd43e8fcf2492f57dacc298
+?
+UberApp/urls.py,d\f\df05340fd93af59b4f6716bd5f7b76107ac29f08
---
+++ taxi-app/server/taxi/trips/tests/test_http.py
@@ -0,0 +1,35 @@
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+
+PASSWORD = 'pAssw0rd!'
+
+
+# new
+def create_user(username='user@example.com', password=PASSWORD):
+    return get_user_model().objects.create_user(
+        username=username, password=password)
+
+
+class AuthenticationTest(APITestCase):
+
+    # Function collapsed for clarity.
+    def test_user_can_sign_up(self): ...
+
+    # new
+    def test_user_can_log_in(self):
+        user = create_user()
+        response = self.client.post(reverse('log_in'), data={
+            'username': user.username,
+            'password': PASSWORD,
+        })
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(response.data['username'], user.username)
+
+    # new
+    def test_user_can_log_out(self):
+        user = create_user()
+        self.client.login(username=user.username, password=PASSWORD)
+        response = self.client.post(reverse('log_out'))
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
